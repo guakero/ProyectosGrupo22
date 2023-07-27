73a093325b7e9819729afddf5e9e8c6399be2b05
guakero <116929201+guakero@users.noreply.github.com>
2023-07-27 18:21:12 -0500

    update

--- app/src/main/java/com/example/proyectosgrupo22/MainActivity.java
+++ app/src/main/java/com/example/proyectosgrupo22/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.proyectosgrupo22;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.session_32);
+        TextView textvew1=findViewById(R.id.textoenpantalla);
+        Animation mianimacion =AnimationUtils.loadAnimation(getApplicationContext(),R.anim.animacion2);
+        textvew1.startAnimation(mianimacion);
     }
 }
