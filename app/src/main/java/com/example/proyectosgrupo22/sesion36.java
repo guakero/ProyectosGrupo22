85f590dbd0a4102882d5c06618e70cc650a21e18
guakero <116929201+guakero@users.noreply.github.com>
2023-08-01 20:15:12 -0500

    2

--- app/src/main/java/com/example/proyectosgrupo22/sesion36.java
+++ app/src/main/java/com/example/proyectosgrupo22/sesion36.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -14,10 +16,25 @@ public class sesion36 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sesion36);
 
+
     }
 
     public void MostrarElTexto(View view){
         EditText cajaDeTexto = findViewById(R.id.etCajaDeTexto);
         Toast.makeText(getApplicationContext(),cajaDeTexto.getText(),Toast.LENGTH_LONG).show();
     }
+
+    public void obternerForma (View view){
+        CheckBox opcionCarro = findViewById(R.id.opCarro);
+        CheckBox opcionMoto = findViewById(R.id.opMoto);
+        CheckBox opcionBici = findViewById(R.id.opBici);
+
+        String carro = "\nCarro:"+opcionCarro.isChecked();
+        String moto = "\nMoto:"+opcionMoto.isChecked();
+        String bici = "\nBici:"+opcionBici.isChecked();
+
+        Toast.makeText(getApplicationContext(),carro+moto+bici,Toast.LENGTH_LONG).show();
+
+    }
+
 }
