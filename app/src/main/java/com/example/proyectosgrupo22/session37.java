34dd6c5b4d21c1df558af474517178e73fda287a
guakero <116929201+guakero@users.noreply.github.com>
2023-08-02 19:59:21 -0500

    session37

--- app/src/main/java/com/example/proyectosgrupo22/session37.java
+++ app/src/main/java/com/example/proyectosgrupo22/session37.java
@@ -1,13 +1,14 @@
 package com.example.proyectosgrupo22;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class session37 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.session37);
     }
 }
